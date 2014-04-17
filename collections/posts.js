cf2a9832cd26734d60fd4f880402f7a2bd682926
Edaan <Edaan.Getzel@gmail.com>
2014-04-16 18:46:24 -0700

    changed posts

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,3 @@
+// synchronizes real time data to between PUBLICATIONS and FIXTURES
+// no 'var' is needed for posts in order to exapnd scope to entire project
 Posts = new Meteor.Collection('posts');
