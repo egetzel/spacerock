d8514ab724c672a45fb482dddde7ea619a95db76
Edaan <Edaan.Getzel@gmail.com>
2014-04-18 15:35:39 -0700

    cleaning up

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,5 @@
 //return a cursor to reference all posts in DB
 // flagged param excludes items that have been flagged
 Meteor.publish('posts', function(){
-	return Posts.find({flagged:'false'});
+	return Posts.find();
 });
