b4202dd12763670c984dfde0a6038b09c957221a
Edaan <Edaan.Getzel@gmail.com>
2014-04-17 22:13:58 -0700

    update collections

--- collections/posts.js
+++ collections/posts.js
@@ -1,3 +1,10 @@
 // synchronizes real time data to between PUBLICATIONS and FIXTURES
 // no 'var' is needed for posts in order to exapnd scope to entire project
 Posts = new Meteor.Collection('posts');
+//allows for client side inserts after setting insecure to zero
+Posts.allow({
+	insert: function(userId, doc){
+		//only allow posting if you are logged in
+		return !! userId;
+	}
+});
