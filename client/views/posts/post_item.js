ced27fc4064153bc78113de7ddbfc1ec9df30232
Edaan <Edaan.Getzel@gmail.com>
2014-04-18 15:33:25 -0700

    add editing post functionality

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,5 +1,8 @@
 Template.postItem.helpers({
-	domain: function(){
+	ownPost: function(){
+		return this.userId == Meteor.userId();
+	},
+	domain: function() {
 		var a = document.createElement('a');
 		a.href = this.url;
 		return a.hostname;
