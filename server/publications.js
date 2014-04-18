a993dcb07ea275e5976e7f80b612584de5d92f99
Edaan <Edaan.Getzel@gmail.com>
2014-04-17 22:10:58 -0700

    update templates block out console access to server

--- server/publications.js
+++ server/publications.js
@@ -1,4 +1,5 @@
 //return a cursor to reference all posts in DB
+// flagged param excludes items that have been flagged
 Meteor.publish('posts', function(){
-	return Posts.find();
+	return Posts.find({flagged:'false'});
 });
