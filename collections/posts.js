865aa6418c767d5daeba7af3cfdb3d686108521b
Edaan <Edaan.Getzel@gmail.com>
2014-04-20 18:25:43 -0700

    update posts.js

--- collections/posts.js
+++ collections/posts.js
@@ -36,11 +36,26 @@ Meteor.methods({
 			userId: user._id,
 			author: user.username,
 			submitted: new Date().getTime(),
-			commentsCount: 0
+			commentsCount: 0,
+			upvoters: [],
+			votes: 0
 		});
 
 
 		var postId = Posts.insert(post);
 		return postId;
+	},
+	upvotes: function(postId) {
+		var user = Meteor.user();
+		if (!user)
+			throw new Meteor.Error(401, 'You need to login to upvote');
+		
+		Posts.update({
+			_id: postId,
+			upvoters: {$ne: user._id}
+		}, {
+			$addToSet: {upvoters: user._id},
+			$inc: {votes:1}
+		});
 	}
 });
